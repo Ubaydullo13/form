2fdf9d7b412b8bca48126de202fafd423b189d4e
Ubaydullo13 <solizhonov98@gmail.com>
2024-01-21 17:51:02 +0500

    first commit

---
+++ functions.js
@@ -0,0 +1,33 @@
+function validate(name, price, category) {
+  if (!name.value || name.value.trim().length < 3) {
+    alert("Name length must be greater than 3 characters");
+    focus();
+    return false;
+  }
+  if (!price.value || isNaN(Number(price.value))) {
+    alert("Price must be a number");
+    focus();
+    return false;
+  }
+  if (!category.value) {
+    alert("Category is required"); 
+    focus();
+    return false;
+  }
+  return true;
+}
+
+function createRow(phone, index) {
+  return `<tr>
+  <td>${index}</td>
+  <td>${phone.name}</td>
+  <td>${phone.price}</td>
+  <td>${phone.category}</td>
+  <td data-id = 'data_${phone.id}'>
+  <button class="btn btn-primary btn-sm">Edit</button>
+  <button class="btn btn-danger btn-sm">Delete</button>
+</td>
+  `;
+}
+
+export { validate, createRow };
---
+++ index.js
@@ -0,0 +1,61 @@
+import {validate, createRow} from './functions.js';
+
+const button = document.getElementById('button');
+const name = document.querySelector('#name');
+const price = document.querySelector('#price');
+const category = document.querySelector('#category');
+const tbody = document.querySelector('#tbody');
+const form = document.querySelector('form');
+
+
+// Load existing data from LocalStorage
+const existingData = JSON.parse(localStorage.getItem('phones')) || [];
+
+form.addEventListener('submit', (e) => {
+e.preventDefault();
+
+if(validate(name, price, category)){
+  let phone = {
+    id: Date.now(),
+    name: name.value,
+    price: price.value,
+    category: category.value,
+  };
+
+   // Add the new phone to the existing data array
+   existingData.push(phone);
+
+   // Save the new data to LocalStorage
+   localStorage.setItem('phones', JSON.stringify(existingData));
+  let tr = createRow(phone, existingData.length);
+tbody.innerHTML += tr;
+  form.reset();
+}else{
+  console.log("Failed check");
+}
+});
+
+document.addEventListener('DOMContentLoaded', function(){
+// Populate the table with existing data
+existingData.forEach((phone, index) => {
+  let tr = createRow(phone, index + 1);
+  tbody.innerHTML += tr;
+});
+let deleteBtn = document.querySelectorAll('span.btn-danger');
+
+deleteBtn.length && deleteBtn.forEach(del => {
+  del && del.addEventListener('click', function() {
+    let isDelete = confirm('Are you sure you want to delete this phone?');
+    if(isDelete){
+      let id = this.parentNode.getAttribute('data-id').substring(5);
+      existingData.existingData.filter(phone => {
+        return phone.id != id;
+      })
+      existingData.splice(index, 1);
+      localStorage.setItem('phones', JSON.stringify(existingData));
+      this.parentNode.parentNode.remove();
+    }
+})
+});
+
+});
